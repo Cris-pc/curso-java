71ac6be81fc6c3db13e58464b3f2abbb6c7c1bf4
Cristhian Flores <cfcatupe2109@gmail.com>
2025-11-24 23:31:01 -0500

    ejercicio1 seccion 2

---
+++ Introduccion-Seccion2/src/EjercicioPresentate.java
@@ -0,0 +1,7 @@
+public class EjercicioPresentate {
+    public static void main(String[] args) {
+        System.out.println("Nombre Completo: Cristhian Flores");
+        System.out.println("Edad: 36");
+        System.out.println("Pais: Ecuador");
+    }
+}
